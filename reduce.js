42edbfc8a91a0c24c773e942683b8857726d14a6
Grenyerre <141226154+Grenyerre@users.noreply.github.com>
2024-08-28 17:25:29 +0000

    Complete reduce method challenge

--- reduce.js
+++ reduce.js
@@ -6,7 +6,28 @@
 
 
 // Summing an array of numbers:
-
+const nums = [0, 1, 2, 3, 4];
+let sum = nums.reduce((acc, curr) => {
+// acc is the accumulator and curr is the currentValue
+  console.log(
+    "Accumulator:", acc,
+    "Current Value:", curr,
+    "Total:", acc + curr
+  );
+  return acc + curr;
+});
+console.log(sum);
+/**
+ * Accumulator: 0 Current Value: 1 Total: 1
+ * Accumulator: 1 Current Value: 2 Total: 3
+ * Accumulator: 3 Current Value: 3 Total: 6
+ * Accumulator: 6 Current Value: 4 Total: 10
+ * 10
+ * Why did the function only execute 4 times?
+ * The reduce method actually takes a second parameter,
+ * the initial value to use as the accumulator. If this 
+ * is not specified the accumulator will default to the first value
+ */
 
 const teamMembers = [
   {
@@ -32,6 +53,23 @@ const teamMembers = [
 ];
 
 // Totaling a specific object property
-
+let totalExperience = teamMembers.reduce((acc, curr) => acc + curr.yrsExperience, 0);
+console.log(totalExperience);
 
 // Grouping by a property, and totaling it too
+/**
+ * The 1st set of curly braces are the boundaries of the callback function.
+ * The 2nd set, after the comma, is the initial value, an empty array.
+ */
+let experienceByProfession = teamMembers.reduce((acc, curr) => {
+  let key = curr.profession;
+  if (!acc[key]) {
+    acc[key] = curr.yrsExperience;
+  } else {
+    acc[key] += curr.yrsExperience;
+  }
+  return acc;
+  }, {});
+
+  console.log(experienceByProfession);
+  
